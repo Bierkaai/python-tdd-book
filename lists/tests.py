444f87fa19b464f5be13a6db2deb3ae64579c687
Coen Jonker <coen@datawizards.nl>
2023-01-19 13:49:27 +0100

    Adding functional test and functionality for displaying entered text in table

--- lists/tests.py
+++ lists/tests.py
@@ -7,17 +7,20 @@ from django.template.loader import render_to_string
 from lists.views import home_page
 
 
+NEW_ITEM_TEXT = "A new list item"
+
+
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve("/")
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
+    def test_home_page_uses_correct_template(self):
         response = self.client.get("/")
-
-        html = response.content.decode("utf8")
-        self.assertTrue(html.startswith("<html>"))
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.strip().endswith("</html>"))
-
         self.assertTemplateUsed(response, "home.html")
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post("/", data={"item_text": NEW_ITEM_TEXT})
+        self.assertIn(NEW_ITEM_TEXT, response.content.decode())
+        # should still be using the home template
+        self.assertTemplateUsed(response, 'home.html')
