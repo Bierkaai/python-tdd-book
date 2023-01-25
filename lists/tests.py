844ffcfd57857c9fba9bdc6e782f6b1742788556
Coen Jonker <coen@datawizards.nl>
2023-01-25 11:44:23 +0100

    model changed and database migration added

--- lists/tests.py
+++ lists/tests.py
@@ -6,7 +6,7 @@ from django.template.loader import render_to_string
 
 from lists.views import home_page
 
-from lists.models import Item
+from lists.models import Item, List
 
 NEW_ITEM_TEXT = "A new list item"
 
@@ -24,15 +24,23 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, "home.html")
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = FIRST_ITEM_TEXT
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = SECOND_ITEM_TEXT
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -41,7 +49,9 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
 
         self.assertEqual(first_item.text, FIRST_ITEM_TEXT)
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_item.text, SECOND_ITEM_TEXT)
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -50,8 +60,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, "list.html")
 
     def test_displays_all_items(self):
-        Item.objects.create(text=FIRST_ITEM_TEXT)
-        Item.objects.create(text=SECOND_ITEM_TEXT)
+        list_ = List.objects.create()
+        Item.objects.create(text=FIRST_ITEM_TEXT, list=list_)
+        Item.objects.create(text=SECOND_ITEM_TEXT, list=list_)
 
         response = self.client.get("/lists/the-only-list-in-the-world/")
 
